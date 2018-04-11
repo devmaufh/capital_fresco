2f52e1da47581783462cde08dbb88e08f3e8f157
Mauricio Flores Hernández <mau1361317@gmail.com>
2018-04-10 23:17:44 -0500

    Se agregó persistencia de datos, verificación de casillas de texto en el Login y el Registro

--- app/src/main/java/com/example/lsaal/cf/Login.java
+++ app/src/main/java/com/example/lsaal/cf/Login.java
@@ -1,13 +1,17 @@
 package com.example.lsaal.cf;
+//Pendiente: Modificar JSON para extraer todos los datos de tabla users
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.text.InputType;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -26,50 +30,40 @@ public class Login extends AppCompatActivity implements Response.Listener<JSONOb
     usuario usr= new usuario();//Para salvar datos
 
     //Para conexion
-    String ip="192.168.1.228";
+    String ip="192.168.1.75";
     RequestQueue rq;
     JsonRequest jrq;
     //
     private EditText inicio;
-    EditText pass;
+    private EditText pass;
     private Button btnLogin,btnRegister;
-    ProgressBar pb;
+    private Switch switchRember;
+
+    private SharedPreferences prefs; //Persistencia de datos.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        pb=(ProgressBar)findViewById(R.id.pbLoading);
-        inicio = findViewById(R.id.txtUser);
-        pass=findViewById(R.id.txtPassword);
-        btnLogin=(Button)findViewById(R.id.btnLogin);
-        btnRegister=(Button)findViewById(R.id.btnRegister);
+        prefs=getSharedPreferences("Preferences", Context.MODE_PRIVATE); //Para lectura
         rq= Volley.newRequestQueue(getApplicationContext());
-
+        bindUI();
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(Login.this, Registro.class);
-                startActivity(i);
+               launchRegister();
             }
         });
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //ProgresBar
-                pb.setVisibility(View.VISIBLE);
+                String email=inicio.getText().toString();
+                String password=pass.getText().toString();
+                if(isValidData(email,password)){
+                    login();
+                }
 
-                //
-                login();
-
-            }
-        });
-
-        btnRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setContentView(R.layout.activity_registro);
             }
         });
     }
@@ -78,23 +72,26 @@ public class Login extends AppCompatActivity implements Response.Listener<JSONOb
     public void onErrorResponse(VolleyError error) {
         //Conexion error
         Toast.makeText(getApplicationContext(),"Error usuario o contraseña incorrectos",Toast.LENGTH_SHORT).show();
-
     }
-
     @Override
     public void onResponse(JSONObject response) {
         //Conexion Succesfull
         JSONArray jsonArray= response.optJSONArray("datos");
         JSONObject jsonObject=null;
         Toast.makeText(getApplicationContext(),"Inicio de sesión correcto",Toast.LENGTH_SHORT).show();
-
-
         try {
             jsonObject=jsonArray.getJSONObject(0);
-            usr.setCorreo(jsonObject.optString("correo"));//Guarda los datos en objeto para salvar el user
-            usr.setContraseña(jsonObject.optString("contraseña"));
-            Toast.makeText(getApplicationContext(),"Correo: "+usr.getCorreo(),Toast.LENGTH_SHORT).show();
-            setContentView(R.layout.activity_registro);
+            String ine=jsonObject.optString("clave_ine");
+            String curp=jsonObject.optString("curp");
+            String nombre=jsonObject.optString("nombre");
+            String apellidos=jsonObject.optString("apellios");
+            String fecha=jsonObject.optString("fecha_nac");
+            String direccion=jsonObject.optString("direccion");
+            String telefono=jsonObject.optString("telefono");
+            String correo=jsonObject.optString("correo");
+            String aceptado=jsonObject.optString("aceptado");
+            goToMain();
+            saveDataOnPreferences(ine,curp,nombre,apellidos,fecha,direccion,telefono,correo,aceptado);//Aquí se
         }catch (JSONException e){
             e.printStackTrace();
         }
@@ -104,6 +101,58 @@ public class Login extends AppCompatActivity implements Response.Listener<JSONOb
                 "&contraseña="+pass.getText().toString();
         jrq= new JsonObjectRequest(Request.Method.GET,url,null,this,this);
         rq.add(jrq);
+    }
+    private void bindUI(){
+        inicio=(EditText) findViewById(R.id.txtUser);
+        pass=(EditText)findViewById(R.id.txtPassword);
+        btnLogin=(Button)findViewById(R.id.btnLogin);
+        btnRegister=(Button)findViewById(R.id.btnRegister);
+        switchRember=(Switch)findViewById(R.id.switchRemember);
+
+    }
+
+    private boolean isValidEmail(String email){//Valida el email
+        return !TextUtils.isEmpty(email)&& Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+    private boolean isValidPassword(String password){ //Valída la contraseña
+        return password.length()>4;
+    }
+    private boolean isValidData(String email, String password){
+        if(!isValidEmail(email)){
+            Toast.makeText(getApplicationContext(),"El correo no es válido, intentelo nuevamente",Toast.LENGTH_SHORT).show();
+            return false;
+        }else if(!isValidPassword(password)){
+            Toast.makeText(getApplicationContext(),"La contraseña debe contener más de 4 caractéres",Toast.LENGTH_SHORT).show();
+            return false;
+        }else{
+            return true;
+        }
+    }
+    private void goToMain(){
+        //Mandar a pantalla principal, o home.
+    }
+    private void saveDataOnPreferences(String ine,String curp,String nombre,String apellidos,
+                                       String fecha,String direccion,String telefono,String
+                                               correo,String status){//Guarda los datos en el archivo Preferences
+        if(switchRember.isChecked()){
+            SharedPreferences.Editor editor= prefs.edit();//Para escribir
+            editor.putString("ine",ine);
+            editor.putString("curp",curp);
+            editor.putString("nombre",nombre);
+            editor.putString("apellidos",apellidos);
+            editor.putString("fecha",fecha);
+            editor.putString("direccion",direccion);
+            editor.putString("telefono",telefono);
+            editor.putString("correo",correo);
+            editor.putString("status",status);
+            editor.commit();//Detiene el código hasta que se guarden los datos en el archivo preferences
+           // editor.apply(); //Continua la ejecución del código se guarden o no los datos
+            Toast.makeText(this,"Datos guardados en preferences",Toast.LENGTH_SHORT).show();
+
+        }
+    }
+    private void launchRegister(){
+        startActivity(new Intent(this,Registro.class));
     }
 
 
--- app/src/main/java/com/example/lsaal/cf/Registro.java
+++ app/src/main/java/com/example/lsaal/cf/Registro.java
@@ -1,34 +1,52 @@
 package com.example.lsaal.cf;
 
-import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.text.method.TextKeyListener;
-import android.view.KeyEvent;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import javax.microedition.khronos.egl.EGLDisplay;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
 
-public class Registro extends AppCompatActivity {
+import org.json.JSONObject;
+
+public class Registro extends AppCompatActivity implements Response.Listener<JSONObject>, Response.ErrorListener {
+    private RequestQueue rq;
+    private JsonRequest jrq;
     private EditText edName,edApellidos,edCurp,edIne,edFecha,edDireccion,edCorreo,edTelefono,edContraseña;
     private Button btnRegistrar, btnUbicacion;
-    private Register_Users registra;
+    private String ip="192.168.1.75";
+    private SharedPreferences prefs; //Persistencia de datos.
+    private String ine,curp,nombre,apellidos,fecha,direccion,telefono,correo,contraseña;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
-        //EditText
+        prefs=getSharedPreferences("Preferences", Context.MODE_PRIVATE); //Para lectura
+        bindUI();
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                carga_webService();
+
+            }
+        });
+    }
+    public void bindUI(){
         edName=(EditText)findViewById(R.id.edtNombre);
         edApellidos=(EditText)findViewById(R.id.edtApellidos);
         edCurp=(EditText)findViewById(R.id.edtCurp);
@@ -40,18 +58,102 @@ public class Registro extends AppCompatActivity {
         edContraseña=(EditText)findViewById(R.id.edtContraseña);
         //Botones
         btnUbicacion = findViewById(R.id.ubicacion);
-        btnRegistrar=(Button)findViewById(R.id.btnRegister);
+        btnRegistrar=(Button)findViewById(R.id.btnEnviar_D);
+        rq= Volley.newRequestQueue(getApplicationContext());
+    }
 
-        btnRegistrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                registra=new Register_Users(getApplicationContext(),
-                        edIne.getText().toString(), edCurp.getText().toString(),edName.getText().toString()
-                        ,edApellidos.getText().toString(),edFecha.getText().toString(),edDireccion.getText().toString()
-                        ,edTelefono.getText().toString(),edCorreo.getText().toString(),edContraseña.getText().toString());
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        Toast.makeText(this,"Error al registrar",Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onResponse(JSONObject response) {
+        Toast.makeText(this,"Cuenta registrada correctamente",Toast.LENGTH_SHORT).show();
+        goToMain();
+        saveDataOnPreferences(ine,curp,nombre,apellidos,fecha,direccion,telefono,correo,"f");
+        Toast.makeText(this,"Datos almacenados en preferences", Toast.LENGTH_LONG).show();
 
-            }
-        });
+    }
+    public boolean isValidCurp(String curp){
+        return !TextUtils.isEmpty(curp)&&curp.length()==18;
+    }
+    public boolean isValidIne(String ine){
+        return !TextUtils.isEmpty(ine)&&ine.length()==14;
+    }
+    public boolean isValidFecha(String fecha){
+        return TextUtils.isEmpty(fecha);
+    }
+    private boolean isValidEmail(String email){//Valida el email
+        return !TextUtils.isEmpty(email)&& Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+    private boolean isValidPassword(String password){ //Valída la contraseña
+        return password.length()>4;
+    }
+    private boolean isValidNumber(String number){
+        return !TextUtils.isEmpty(number)&&number.length()==10;
+    }
+    private boolean isValidAllData(String curp, String ine,String fecha,String email,String password, String number){
 
+        if(!isValidCurp(curp)){
+            Toast.makeText(this,"La curp no es válida, debe contener 18 caracteres",Toast.LENGTH_SHORT).show();
+            return false;
+        }else if(!isValidIne(ine)){
+            Toast.makeText(this,"EL código CIC INE no es válido, debe contener 14 caracteres",Toast.LENGTH_SHORT).show();
+            return false;
+        }else if(!isValidFecha(fecha)){
+            Toast.makeText(this,"La fecha debe contener un formato correcto", Toast.LENGTH_LONG).show();
+            return false;
+        }else if(!isValidEmail(email)){
+            Toast.makeText(this,"El email debe contener un formato válido\nEjemplo: sujeto@domain.com",Toast.LENGTH_SHORT).show();
+            return false;
+        }else if(isValidNumber(number)){
+            Toast.makeText(this,"El número telefónico debe contener 10 digitos", Toast.LENGTH_LONG).show();
+            return false;
+        }else if(!isValidPassword(password)){
+            Toast.makeText(this,"La contraseña debe contener más de 4 caracteres",Toast.LENGTH_SHORT).show();
+            return false;
+        }else {
+            return true;
+        }
+    }
+    private void carga_webService(){
+        ine=edName.getText().toString();
+        curp=edCurp.getText().toString();
+        nombre=edName.getText().toString();
+        apellidos=edApellidos.getText().toString();
+        fecha=edFecha.getText().toString();
+        direccion=edDireccion.getText().toString();
+        telefono=edTelefono.getText().toString();
+        correo=edCorreo.getText().toString();
+        contraseña=edContraseña.getText().toString();
+        if(isValidAllData(curp,ine,fecha,correo,contraseña,telefono)) {
+            String url = "http://" + ip + "/capital/RegisterNewUser.php" +
+                    "?ine=+" + ine + "+&curp=" + curp + "&nombre=" + nombre + "&apellidos=" + apellidos + "" +
+                    "&fecha=" + fecha + "&direccion=" + direccion + "&telefono=" + telefono + "&correo=" + correo + "&contraseña=" + contraseña;
+
+            url = url.replace(" ", "%20");
+            jrq = new JsonObjectRequest(Request.Method.GET, url, null, this, this);
+            rq.add(jrq);
+        }
+    }
+    private void saveDataOnPreferences(String ine,String curp,String nombre,String apellidos,
+                                       String fecha,String direccion,String telefono,String
+                                               correo,String status){//Guarda los datos en el archivo Preferences
+        SharedPreferences.Editor editor= prefs.edit();//Para escribir
+        editor.putString("ine",ine);
+        editor.putString("curp",curp);
+        editor.putString("nombre",nombre);
+        editor.putString("apellidos",apellidos);
+        editor.putString("fecha",fecha);
+        editor.putString("direccion",direccion);
+        editor.putString("telefono",telefono);
+        editor.putString("correo",correo);
+        editor.putString("status",status);
+        editor.commit();//Detiene el código hasta que se guarden los datos en el archivo preferences
+            // editor.apply(); //Continua la ejecución del código se guarden o no los datos
+            Toast.makeText(this,"Datos guardados en preferences",Toast.LENGTH_SHORT).show();
+    }
+    private void goToMain(){
+        //Mandar a pantalla principal
     }
 }
