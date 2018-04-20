0fd4e61553fd60ddc271d615376655f0de1ebf1c
Lalopa <16031227@itcelaya.edu.mx>
2018-04-19 20:12:02 -0500

    Cards

--- app/src/main/java/com/example/lsaal/cf/Agricultor.java
+++ app/src/main/java/com/example/lsaal/cf/Agricultor.java
@@ -2,12 +2,51 @@ package com.example.lsaal.cf;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.lsaal.cf.Adapters.MyAdapterHectarea;
+import com.example.lsaal.cf.Clases_Datos.hectareas;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Agricultor extends AppCompatActivity {
 
-    @Override
+    private List<hectareas> hectareas;
+
+    private RecyclerView recycler_agricultor;
+    private RecyclerView.Adapter agriAdapter;
+    private RecyclerView.LayoutManager agriManager;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agricultor);
+
+        hectareas = getHectareas();
+
+        recycler_agricultor = findViewById(R.id.recycler_agricultor);
+        agriManager = new LinearLayoutManager(this);
+
+        agriAdapter = new MyAdapterHectarea(hectareas, R.drawable.card_agricultor, new MyAdapterHectarea.OnItemClickListener() {
+            @Override
+            public void onItemClick(hectareas hect, int position) {
+
+            }
+        });
+        recycler_agricultor.setHasFixedSize(true);
+        recycler_agricultor.setItemAnimator(new DefaultItemAnimator());
+        recycler_agricultor.setAdapter(agriAdapter);
+        recycler_agricultor.setLayoutManager(agriManager);
+
+
     }
+
+    private List<hectareas> getHectareas(){
+        return new ArrayList<hectareas>(){{
+            add(new hectareas());
+        }};
+    }
+
 }
